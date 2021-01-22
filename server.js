340aa1dbef9d25e214b720c032c9871e4485106e
Aleksandra Joachimiak <joachim07@poczta.onet.pl>
2021-01-22 18:15:02 +0100

    Modify server.js

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
